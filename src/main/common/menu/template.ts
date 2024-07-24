09ec4a003c6371d099f7b91a93176078bdfb75d4
烟雀 <yanquer@qq.com>
2024-07-24 10:21:29 +0800

    u template

--- src/main/common/menu/template.ts
+++ src/main/common/menu/template.ts
@@ -1,8 +1,9 @@
-
-
-import {MenuItemConstructorOptions, MenuItem, app} from 'electron';
+import {app, MenuItem, MenuItemConstructorOptions} from 'electron';
 import {forceQuit, getAboutMenuItem} from "./menu-items";
 import {MenuNames} from "./menu-names";
+import {getServiceBySymbol} from "../../../common/container/inject-container";
+import {IWindowsManager} from "../../electron/service";
+import {WindowNames} from "../defines";
 
 
 export const getMenuTemplate = async (): Promise<Array<(MenuItemConstructorOptions) | (MenuItem)>> => [
@@ -10,13 +11,19 @@ export const getMenuTemplate = async (): Promise<Array<(MenuItemConstructorOptio
         id: MenuNames.startRecordPage,
         label: `启动录制`,
         click: () => {
-            app.focus();
+            const winManager = getServiceBySymbol<IWindowsManager>(IWindowsManager)
+            const capWin = winManager.getWinById(WindowNames.CaptureWin)
+            if (capWin) {
+                app.focus();
+                capWin.open()
+            }
         }
     },
     {
         id: MenuNames.settingView,
         label: `设置`,
         click: () => {
+            // todo: 设置视图
             app.focus();
         }
     },
