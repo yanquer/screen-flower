d6d01b833f7ce47256eb72df583ef9e5dd0d5dbb
烟雀 <yanquer@qq.com>
2025-03-04 17:55:27 +0800

    清除注释

--- resources/scripts/auto-set-version.js
+++ resources/scripts/auto-set-version.js
@@ -6,15 +6,10 @@ const path = require('path');
 const projectDir = path.join(__dirname, '..', '..');
 
 const getLatest = () => {
-  // 读取 readme.rst 文件
   const readmePath = path.join(projectDir, 'CHANGELOG.rst');
   const readmeContent = fs.readFileSync(readmePath, 'utf8');
 
-  // 解析 .rst 文件
-  // const parsedRst = rst.parse(readmeContent);
-
-  // 假设版本号在 .rst 文件的某个标题或段落中
-  // 这里假设版本号在类似 "Version: 1.2.3" 的段落中
+  // 顶级标题第一个
   const versionRegex = /\s*(\d+\.\d+\.\d+)\n=+/;
   const versionMatch = readmeContent.match(versionRegex);
 
@@ -23,9 +18,7 @@ const getLatest = () => {
     process.exit(1);
   }
 
-  const newVersion = versionMatch[1];
-
-  return newVersion;
+  return versionMatch[1];
 }
 
 
